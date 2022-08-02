c02b729736095533d4cf4fd89361843884a67661
Giovanni Michel <geo4581@gmail.com>
2022-08-01 22:12:46 -0600

    Update Signup.js

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -11,14 +11,17 @@ export default function Signup() {
   const [firstName, setFirstname] = useState('')
   const [LastName, setLastname] = useState('')
   const [dob, setDob] = useState('')
+  const [addy, setAddy] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [zip, setZip] = useState('')
   const [thumbnail, setThumbnail] = useState(null)
-
   const [thumbnailError, setThumbnailError] = useState(null)
   const { signup, isPending, error } = useSignup()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    signup(email, password, firstName, LastName, dob, displayName, thumbnail)
+    signup(email, password, firstName, LastName, dob, displayName, addy, city, state, zip, thumbnail)
   }
 
     const handleFileChange = (e) => {
@@ -100,7 +103,42 @@ export default function Signup() {
           value={displayName}
         />
       </label>
-
+  <label>
+        <span>Residential Address:</span>
+        <input
+          required
+          type="text" 
+          onChange={(e) => setAddy(e.target.value)} 
+          value={addy}
+        />
+      </label>
+      <label>
+        <span>city:</span>
+        <input
+          required
+          type="text" 
+          onChange={(e) => setCity(e.target.value)} 
+          value={city}
+        />
+      </label>
+      <label>
+        <span>State:</span>
+        <input
+          required
+          type="text" 
+          onChange={(e) => setState(e.target.value)} 
+          value={state}
+        />
+      </label>
+      <label>
+        <span>Zip code:</span>
+        <input
+          required
+          type="text" 
+          onChange={(e) => setZip(e.target.value)} 
+          value={zip}
+        />
+      </label>
       <label>
         <span>Profile thumbnail:</span>
         <input 
