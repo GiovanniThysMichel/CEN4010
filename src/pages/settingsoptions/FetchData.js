a40796b2cd8f6d5989a3150a4207a3d7b66cc30f
Tyman995 <107812601+Tyman995@users.noreply.github.com>
2022-08-02 01:55:15 -0400

    Add files via upload

--- src/pages/settingsoptions/FetchData.js
+++ src/pages/settingsoptions/FetchData.js
@@ -1,13 +1,20 @@
-import { projectAuth } from '../../firebase/config'
+import { useState } from 'react'
+import { projectAuth, projectFirestore } from '../../firebase/config'
 import { useDocument } from '../../hooks/useDocument'
 import styles from './Settings.module.css'
 
 export default function FetchData(){
+
+
+    const [firstName, setfirstName] = useState('')
+    const [LastName, setLastName] = useState('')
+    const [address, setAddress] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setSta] = useState('')
+    const [zip, setZip] = useState('')
+
     const { uid } = projectAuth.currentUser
     const { error, document } = useDocument('users', uid)
-    
-
-    console.log(uid)
     
     if(error){
         return <div className="error">{error}</div>
@@ -16,41 +23,96 @@ export default function FetchData(){
         return <div className="loading">Loading...</div>
     }
 
+
+    const updateUser = async (uid, updates) => {
+        console.log("ENTERED FUNCTION")
+        console.log(firstName)
+        console.log(LastName)
+        await projectFirestore.collection("users").doc(uid).update(updates);
+        const doc = await projectFirestore.collection("users").doc(uid).get();
+
     
 
-    return(
-       
-        
+        const user = {
+            id: uid,
+            firstName: setfirstName,
+            LastName: setLastName,
+            address: setAddress,
+            city: setCity,
+            state: setSta,
+            zip: setZip
+
+        };
+        console.log(user)
+        return user
+    };
+    
+    const handleSubmit = event => {
+        //prevent page refresh
+        event.preventDefault();
+    
+        console.log('form submitted');
+      };
+
+    return(      
         <div className={styles.container}>
             <div className={styles.content}>
                 <div className={styles.sidebar}>
                     <h3>Update Personal Information</h3>
-                    <form>
-                    <label>Display Name: </label>
-                    <input
-                        type="text"
-                        placeholder={document.displayName}
-                    />
-                    <br></br>
+                    <form onSubmit={handleSubmit}>
                     <label>First Name: </label>
                     <input
                         type="text"
+                        name='firstName'
                         placeholder={document.firstName}
+                        onChange = {(e) => setfirstName(e.target.value)}
                     />
                     <br></br>
                     <label>Last Name: </label>
                     <input
                         type="text"
+                        name='lastName'
                         placeholder={document.LastName}
+                        onChange = {(e) => setLastName(e.target.value)}
                     />
                     <br></br>
-                    <label>DOB: </label>
+                    <label>Address: </label>
                     <input
-                        type="date"
-                        placeholder={document.dob}
+                        type="text"
+                        name='address'
+                        placeholder={document.address}
+                        onChange = {(e) => setAddress(e.target.value)}
                     />
                     <br></br>
-                    <button>{"Update Info"}</button>
+                    <label>City: </label>
+                    <input
+                        type="text"
+                        name='city'
+                        placeholder={document.city}
+                        onChange = {(e) => setCity(e.target.value)}
+                    />
+                    <br></br>
+                    <label>State: </label>
+                    <input
+                        type="text"
+                        name='state'
+                        placeholder={document.state}
+                        onChange = {(e) => setSta(e.target.value)}
+                    />
+                    <br></br>
+                    <label>Zip Code: </label>
+                    <input
+                        type="text"
+                        name='zip'
+                        placeholder={document.zip}
+                        onChange = {(e) => setZip(e.target.value)}
+                    />
+                    <br></br>
+                    <br></br>
+                    <button 
+                    onClick={()=>
+                    updateUser(uid, {firstName, LastName, address, city, state, zip})
+                    }> {"Update Info"} </button>
                     </form>
                 </div>
             </div>
