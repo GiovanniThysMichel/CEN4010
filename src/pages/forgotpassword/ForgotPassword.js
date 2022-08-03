31f111f777e1dbb2120e7030336054657a6a6b2d
Riiz0 <51212226+Riiz0@users.noreply.github.com>
2022-08-02 21:41:39 -0400

    Update ForgotPassword.js

--- src/pages/forgotpassword/ForgotPassword.js
+++ src/pages/forgotpassword/ForgotPassword.js
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
 import { Link } from "react-router-dom"
+import { useAuthContext } from "../../hooks/useAuthContext"
 
 export default function ForgotPassword() {
   const emailRef = useRef()
-  const { resetPassword } = useAuth()
+  const { resetPassword } = useAuthContext()
   const [error, setError] = useState("")
   const [message, setMessage] = useState("")
   const [loading, setLoading] = useState(false)
@@ -35,7 +35,7 @@ export default function ForgotPassword() {
           {message && <Alert variant="success">{message}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
+              <Form.Label>Email:</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
             <Button disabled={loading} className="w-100" type="submit">
